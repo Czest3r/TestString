ad161f41d3854ebe96f813a8bfc0dbca8620c070
Czest3r <uwu.egirl@interia.pl>
2023-11-15 09:38:35 +0100

    drugi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 
+        //jeden string, przenieść dwa znaki na przód tego stringa pod warunkiem że tych znaków jest co najmniej 3 a jeśli nie to zostawić jak jest
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj stringa");
+        String test = scanner.nextLine();
+        System.out.println(test);
+
+        String testowy = new String();
+        if(test.length() >= 3){
+            test = test.substring(-2);
+
+        }
     }
 }
